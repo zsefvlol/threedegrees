802cbe715206f730952d01769f14d9d1adcdb002
lol.wen <zsefvlol@163.com>
2016-01-20 16:25:12 +0800

    develop

--- Application/Api/Controller/UserController.class.php
+++ Application/Api/Controller/UserController.class.php
@@ -8,6 +8,8 @@ class UserController extends RestCommonController {
     public function profile_get(){
         if(!$this->uid)
             $this->responseError(new CommonException('200102'));
+        $uid = I('get.uid', $this->uid);
+        if($uid != $this->uid) $this->others_profile($uid);
         $result = D('User')->find($this->uid);
         $invite_list = D('InviteCode')->where(array('from_uid'=>$this->uid))
             ->join('left join tb_user_info on tb_invite_code.to_uid = tb_user_info.uid')
@@ -26,7 +28,21 @@ class UserController extends RestCommonController {
             $result['user_info'] = D('UserInfo')->where(array('uid'=>$this->uid))->find();
         }
 
+        $like_me = D('Like')->where(array('to_uid'=>$this->uid))
+            ->join('left join tb_user_info on tb_like.from_uid = tb_user_info.uid')
+            ->field('truename,relation,uid')->select();
+        $i_like = D('Like')->where(array('from_uid'=>$this->uid))
+            ->join('left join tb_user_info on tb_like.to_uid = tb_user_info.uid')
+            ->field('truename,relation,uid')->select();
+
+        $result['like_me'] = $like_me;
+        $result['i_like'] = $i_like;
+
         $this->responseSuccess($result);
+    }
+
+    private function others_profile($uid){
+        // @TODO fetch others uid
     }
 
     public function profile_post(){
@@ -42,6 +58,10 @@ class UserController extends RestCommonController {
         D('User')->createUserInfoItem($this->uid);
         D('UserInfo')->save($field_to_save);
         $this->responseSuccess(array('result'=>1));
+    }
+
+    public function list_get(){
+
     }
 
     public function set_uid_for_test(){
