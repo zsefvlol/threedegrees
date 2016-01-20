40843114b324706abde6f9139d08aeb877f566e8
AsterOcclu <asterocclu@gmail.com>
2016-01-20 13:41:41 +0800

    add weui-demo code

---
+++ Application/Home/Controller/DemoController.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Home\Controller;
+
+use Think\Controller;
+
+class DemoController extends Controller {
+
+    public function index(){
+        $this->display();
+    }
+
+}
--- Public/index.php
+++ Public/index.php
@@ -1,9 +1,11 @@
 <?php
-define('APP_DEBUG',True);
+
+define('APP_DEBUG', true);
 define('DOCUMENT_ROOT', dirname(__FILE__));
 define('SITE_PREFIX', 'threedegrees_' );
 define('SITE_TITLE', 'ThreeDegrees' );
-define('SITE_URL', 'http://'.$_SERVER['HTTP_HOST'].'/');
+define('SITE_URL', ($_SERVER['REQUEST_SCHEME'] ?: 'http').'://'.$_SERVER['HTTP_HOST'].'/');
+define('BASE_URL', $_SERVER['SCRIPT_NAME'] ? strtr($_SERVER['SCRIPT_NAME'], array('/index.php' => '')) : '');
 define('SITE_NAME', 'ThreeDegrees');
 define('APP_PATH','../Application/');
 require '../ThinkPHP/ThinkPHP.php';
