c42b5d5dd00687f662e26bdc4f9611ee071ab99f
lol.wen <zsefvlol@163.com>
2016-01-20 18:03:50 +0800

    develop

--- Application/Api/Controller/UserController.class.php
+++ Application/Api/Controller/UserController.class.php
@@ -76,7 +76,15 @@ class UserController extends RestCommonController {
         if($result['user_info']['is_single'] != 1){
             $this->responseError(new CommonException('200101'));
         }
+        //不允许单身访问同性的信息
+        if($myUserInfo['is_single']==1 && $result['user_info']['gender'] == $myUserInfo['gender'])
+            $this->responseError(new CommonException('200101'));
         $result['relation_text'] = Degree::getRelationText($this->uid, $uid);
+        D('ViewLog')->add(array(
+            'from_uid'  =>  $this->uid,
+            'to_uid'    =>  $uid,
+            'view_time' =>  time()
+        ));
         $this->responseSuccess($result);
     }
 
@@ -84,6 +92,10 @@ class UserController extends RestCommonController {
         $request = $_REQUEST;
         if(!$this->uid)
             $this->responseError(new CommonException('200102'));
+        $myUser = D('User')->find($this->uid);
+        //非验证用户无法访问
+        if($myUser['verified'] != 1)
+            $this->responseError(new CommonException('200101'));
 
         $allow_fields = array("is_single","truename","relation","gender","birthday","constellation","height","weight","current_location","future_location","month_income","homeland","people","animal","parent","siblings","smoking","drinking","year_before_marriage","child_count","hobby","self_comment","r_age","r_height","r_education","r_location","r_income","r_comment");
         $field_to_save = array('uid'=>$this->uid);
@@ -96,6 +108,34 @@ class UserController extends RestCommonController {
     }
 
     public function list_get(){
+        $request = $_REQUEST;
+        if(!$this->uid)
+            $this->responseError(new CommonException('200102'));
+        $myUser = D('User')->find($this->uid);
+        //非验证用户无法访问
+        if($myUser['verified'] != 1)
+            $this->responseError(new CommonException('200101'));
+        $myUserInfo = D('UserInfo')->find($this->uid);
+        //未选择身份无法访问
+        if($myUserInfo['is_single'] == -1)
+            $this->responseError(new CommonException('200101'));
+        //介绍人身份
+        $filter = array('is_single' =>  '1');
+        if($myUserInfo['is_single'] == 0){
+            //只能访问自己邀请的人的信息
+            $myInvites = D('InviteCode')->where(array('from_uid'=>$this->uid))->select();
+            $myInviteUids = array();
+            foreach ($myInvites as $value)
+                if($value['to_uid'])
+                    $myInviteUids[] = $value['to_uid'];
+            $filter['uid'] = array('in',$myInviteUids);
+        }else{
+            //单身不允许访问同性
+            $filter['gender'] = $myUserInfo['gender'] == '男' ? '女' : '男';
+        }
+        $list = D('UserInfo')->where($filter)->field('truename,birthday,current_location,future_location,hobby,self_comment')->select();
+
+        $this->responseSuccess($list);
 
     }
 
