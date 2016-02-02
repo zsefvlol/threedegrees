b3f302b9ebc13f11f25a930ce6e1c61a3e8081f5
AsterOcclu <asterocclu@gmail.com>
2016-02-02 11:04:30 +0800

    Fix: remove ws code in webpack.config.js

--- Public/index.php
+++ Public/index.php
@@ -1,12 +1,13 @@
 <?php
 
-define('APP_DEBUG',True);
+define('APP_DEBUG', true);
+define('FE_DEBUG', false);
 define('DOCUMENT_ROOT', dirname(__FILE__));
 define('SITE_PREFIX', 'threedegrees_' );
 define('SITE_TITLE', 'ThreeDegrees' );
 define('SITE_URL', 'http://'.$_SERVER['HTTP_HOST'].'/');
 define('SITE_NAME', 'ThreeDegrees');
 define('APP_PATH','../Application/');
-define('STATIC_URL', APP_DEBUG ? 'http://'.$_SERVER['HTTP_HOST'].':8080/static' : '/static');
-define('BASE_URL', APP_DEBUG ? strtr($_SERVER['SCRIPT_NAME'], array('/index.php' => '')) : '');
+define('BASE_URL', strtr($_SERVER['SCRIPT_NAME'], array('/index.php' => '')));
+define('STATIC_URL', FE_DEBUG ? 'http://'.$_SERVER['HTTP_HOST'].':8080/static' : BASE_URL.'/static');
 require '../ThinkPHP/ThinkPHP.php';
