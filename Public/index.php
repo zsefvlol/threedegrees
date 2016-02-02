c889e16929dddf69da04106b97e19bee7d6c8066
lol.wen <zsefvlol@163.com>
2016-02-02 10:54:11 +0800

    change FRONTEND DEBUG

--- Public/index.php
+++ Public/index.php
@@ -1,12 +1,12 @@
 <?php
 
-define('APP_DEBUG',!in_array($_SERVER['HTTP_HOST'],array('love.lolwen.com','love.mengtian.win')));
+define('FRONTEND_DEBUG',false);
 define('DOCUMENT_ROOT', dirname(__FILE__));
 define('SITE_PREFIX', 'threedegrees_' );
 define('SITE_TITLE', 'ThreeDegrees' );
 define('SITE_URL', 'http://'.$_SERVER['HTTP_HOST'].'/');
 define('SITE_NAME', 'ThreeDegrees');
 define('APP_PATH', DOCUMENT_ROOT.'/../Application/');
-define('STATIC_URL', APP_DEBUG ? 'http://'.$_SERVER['HTTP_HOST'].':8080/static' : '/static');
-define('BASE_URL', APP_DEBUG ? strtr($_SERVER['SCRIPT_NAME'], array('/index.php' => '')) : '');
+define('STATIC_URL', FRONTEND_DEBUG ? 'http://'.$_SERVER['HTTP_HOST'].':8080/static' : '/static');
+define('BASE_URL', FRONTEND_DEBUG ? strtr($_SERVER['SCRIPT_NAME'], array('/index.php' => '')) : '');
 require '../ThinkPHP/ThinkPHP.php';
