92ccec28e5de1c9eaa3bb00233a43645dd7cf9b9
lol.wen <zsefvlol@163.com>
2016-02-02 10:19:31 +0800

    revert to old versions

--- Public/index.php
+++ Public/index.php
@@ -6,7 +6,7 @@ define('SITE_PREFIX', 'threedegrees_' );
 define('SITE_TITLE', 'ThreeDegrees' );
 define('SITE_URL', 'http://'.$_SERVER['HTTP_HOST'].'/');
 define('SITE_NAME', 'ThreeDegrees');
-define('APP_PATH','../Application/');
+define('APP_PATH', DOCUMENT_ROOT.'/../Application/');
 define('STATIC_URL', APP_DEBUG ? 'http://'.$_SERVER['HTTP_HOST'].':8080/static' : '/static');
 define('BASE_URL', APP_DEBUG ? strtr($_SERVER['SCRIPT_NAME'], array('/index.php' => '')) : '');
 require '../ThinkPHP/ThinkPHP.php';
