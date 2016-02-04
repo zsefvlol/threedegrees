33244ce877282b91371021134195c79edc700234
AsterOcclu <asterocclu@gmail.com>
2016-02-04 13:17:30 +0800

    add: add remove image function code

--- Frontend/src/lib/JIC.js
+++ Frontend/src/lib/JIC.js
@@ -16,110 +16,116 @@
  */
 
 var jic = {
-        /**
-         * Receives an Image Object (can be JPG OR PNG) and returns a new Image Object compressed
-         * @param {Image} source The source Image Object
-         * @param {Integer} quality The output quality of Image Object
-         * @param {String} output format. Possible values are jpg and png
-         * @return {String} newImageData The compressed Image Object
-         */
 
-        compress: function (source, quality, output_format, max_size) {
-             max_size = max_size || 800;
-             var mime_type = "image/jpeg";
-             if (typeof output_format !== 'undefined' && output_format === 'png') {
-                mime_type = "image/png";
-             }
-            var width  = source.naturalWidth || source.width;
-            var height  = source.naturalHeight || source.height;
-             var cvs = document.createElement('canvas');
-             var max = Math.max(width, height);
-             var zoom = max >= max_size ? max_size / max : 1;
-             cvs.width = Math.round(width * zoom);
-             cvs.height = Math.round(height * zoom);
-             var ctx = cvs.getContext('2d').drawImage(source, 0, 0, cvs.width, cvs.height);
-             var newImageData = cvs.toDataURL(mime_type, quality/100);
-             console.log(newImageData);
-             return newImageData;
-        },
+    /**
+     * Receives an Image Object (can be JPG OR PNG) and returns a new Image Object compressed
+     * @param {Image} source The source Image Object
+     * @param {Integer} quality The output quality of Image Object
+     * @param {String} output format. Possible values are jpg and png
+     * @return {String} newImageData The compressed Image Object
+     */
+    compress: function (source, quality, output_format, max_size) {
+        max_size = max_size || 800;
+        var mime_type = 'image/jpeg';
+        if (typeof output_format !== 'undefined' && output_format === 'png') {
+            mime_type = 'image/png';
+        }
+        var width  = source.naturalWidth || source.width;
+        var height  = source.naturalHeight || source.height;
+        var cvs = document.createElement('canvas');
+        var max = Math.max(width, height);
+        var zoom = max >= max_size ? max_size / max : 1;
+        cvs.width = Math.round(width * zoom);
+        cvs.height = Math.round(height * zoom);
+        var ctx = cvs.getContext('2d').drawImage(source, 0, 0, cvs.width, cvs.height);
+        var newImageData = cvs.toDataURL(mime_type, quality/100);
+        return newImageData;
+    },
 
-        /**
-         * Receives an Image Object and upload it to the server via ajax
-         * @param {Image} compressed_img_obj The Compressed Image Object
-         * @param {String} The server side url to send the POST request
-         * @param {String} file_input_name The name of the input that the server will receive with the file
-         * @param {String} filename The name of the file that will be sent to the server
-         * @param {function} successCallback The callback to trigger when the upload is succesful.
-         * @param {function} (OPTIONAL) errorCallback The callback to trigger when the upload failed.
-         * @param {function} (OPTIONAL) duringCallback The callback called to be notified about the image's upload progress.
-         * @param {Object} (OPTIONAL) customHeaders An object representing key-value  properties to inject to the request header.
-         */
+    /**
+     * Receives an Image Object and upload it to the server via ajax
+     * @param {Image} compressed_img_obj The Compressed Image Object
+     * @param {String} The server side url to send the POST request
+     * @param {String} file_input_name The name of the input that the server will receive with the file
+     * @param {String} filename The name of the file that will be sent to the server
+     * @param {function} successCallback The callback to trigger when the upload is succesful.
+     * @param {function} (OPTIONAL) errorCallback The callback to trigger when the upload failed.
+     * @param {function} (OPTIONAL) duringCallback The callback called to be notified about the image's upload progress.
+     * @param {Object} (OPTIONAL) customHeaders An object representing key-value  properties to inject to the request header.
+     */
+    upload: function(compressed_img_obj, upload_url, file_input_name, filename, successCallback, errorCallback, duringCallback, customHeaders){
 
-        upload: function(compressed_img_obj, upload_url, file_input_name, filename, successCallback, errorCallback, duringCallback, customHeaders){
-
-            var type = "image/jpeg";
-            var data = '';
-            if(filename.substr(-4)==".png"){
-                type = "image/png";
-            }
-            if (XMLHttpRequest.prototype.sendAsBinary === undefined) {
-                XMLHttpRequest.prototype.sendAsBinary = function(string) {
-                    var bytes = Array.prototype.map.call(string, function(c) {
-                        return c.charCodeAt(0) & 0xff;
-                    });
-                    this.send(new Uint8Array(bytes).buffer);
-                };
-            }
-            if (typeof compressed_img_obj !== 'string') {
-                var cvs = document.createElement('canvas');
-                cvs.width = compressed_img_obj.naturalWidth;
-                cvs.height = compressed_img_obj.naturalHeight;
-                var ctx = cvs.getContext("2d").drawImage(compressed_img_obj, 0, 0);
-                //ADD sendAsBinary compatibilty to older browsers
-                data = cvs.toDataURL(type);
-            } else {
-                data = compressed_img_obj;
-            }
-            data = data.replace('data:' + type + ';base64,', '');
-            
-            var xhr = new XMLHttpRequest();
-            xhr.open('POST', upload_url, true);
-            var boundary = 'someboundary';
-
-            xhr.setRequestHeader('Content-Type', 'multipart/form-data; boundary=' + boundary);
+        var type = "image/jpeg";
+        var data = '';
+        if(filename.substr(-4)==".png"){
+            type = "image/png";
+        }
+        if (XMLHttpRequest.prototype.sendAsBinary === undefined) {
+            XMLHttpRequest.prototype.sendAsBinary = function(string) {
+                var bytes = Array.prototype.map.call(string, function(c) {
+                    return c.charCodeAt(0) & 0xff;
+                });
+                this.send(new Uint8Array(bytes).buffer);
+            };
+        }
+        if (typeof compressed_img_obj !== 'string') {
+            var cvs = document.createElement('canvas');
+            cvs.width = compressed_img_obj.naturalWidth;
+            cvs.height = compressed_img_obj.naturalHeight;
+            var ctx = cvs.getContext("2d").drawImage(compressed_img_obj, 0, 0);
+            //ADD sendAsBinary compatibilty to older browsers
+            data = cvs.toDataURL(type);
+        } else {
+            data = compressed_img_obj;
+        }
+        data = data.replace('data:' + type + ';base64,', '');
         
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', upload_url, true);
+        var boundary = 'someboundary';
+
+        xhr.setRequestHeader('Content-Type', 'multipart/form-data; boundary=' + boundary);
+
         // Set custom request headers if customHeaders parameter is provided
         if (customHeaders && typeof customHeaders === "object") {
             for (var headerKey in customHeaders){
                 xhr.setRequestHeader(headerKey, customHeaders[headerKey]);
             }
         }
-        
+
         // If a duringCallback function is set as a parameter, call that to notify about the upload progress
         if (duringCallback && duringCallback instanceof Function) {
-            xhr.onprogress = function (evt) {
-                if (evt.lengthComputable) {  
-                    return (evt.loaded / evt.total)*100;  
+            xhr.upload.onprogress = function progress(e) {
+                if (e.total > 0) {
+                    e.percent = e.loaded / e.total * 100;
                 }
+                duringCallback(e);
             };
         }
 
-            xhr.sendAsBinary(['--' + boundary, 'Content-Disposition: form-data; name="' + file_input_name + '"; filename="' + filename + '"', 'Content-Type: ' + type, '', atob(data), '--' + boundary + '--'].join('\r\n'));
-            
-            xhr.onreadystatechange = function() {
-            if (this.readyState == 4){
-                if (this.status == 200) {
-                    successCallback(this.responseText);
-                }else if (this.status >= 400) {
-                    if (errorCallback &&  errorCallback instanceof Function) {
-                        errorCallback(this.responseText);
-                    }
-                }
+        xhr.onerror = errorCallback;
+
+        function getBody(xhr) {
+            var text = xhr.responseText || xhr.response;
+            if (!text) {
+                return text;
             }
-            };
-
-
+            try {
+                return JSON.parse(text);
+            }   catch (e) {
+                return text;
+            }
         }
+
+        xhr.onload = function onload() {
+            if (xhr.status !== 200) {
+                return errorCallback(xhr);
+            }
+            successCallback(getBody(xhr));
+        };
+
+        xhr.sendAsBinary(['--' + boundary, 'Content-Disposition: form-data; name="' + file_input_name + '"; filename="' + filename + '"', 'Content-Type: ' + type, '', atob(data), '--' + boundary + '--'].join('\r\n'));
+    }
 };
 
 module.exports = jic;
--- Frontend/src/lib/MyUploader.js
+++ Frontend/src/lib/MyUploader.js
@@ -4,9 +4,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Upload from 'rc-upload';
-import { Dialog } from 'react-weui';
+import { Dialog, Toast } from 'react-weui';
 import jic from './JIC';
 import loadImage from './load-image/index';
+import Ajax from '../lib/Ajax';
 const { Alert } = Dialog;
 
 export default class MyUploader extends React.Component {
@@ -18,19 +19,21 @@ export default class MyUploader extends React.Component {
             return photo;
         }) : [],
         showAlert: false,
-        alertContent: ''
+        alertContent: '',
+        showToast: false,
+        toastTimer: null
     }
 
-    uploadHandle = (e) => {
+    componentWillUnmount() {
+        this.state.toastTimer && clearTimeout(this.state.toastTimer);
+    }
 
+
+    startUpload = (e) => {
         const minSize = 1E6;
         var file = e.target.files[0];
-        console.log(file);
         var quality = file.size > minSize ? parseInt((minSize / file.size) * 100) : 100;
-        var options = {
-            noRevoke: true,
-            canvas: false
-        }
+        var options = {noRevoke: true, canvas: false};
         loadImage.parseMetaData(file, (data) => {
            if (data.exif) {
                options.orientation = data.exif.get('Orientation');
@@ -39,64 +42,63 @@ export default class MyUploader extends React.Component {
                file,
                (img) => {
                     var compressd = jic.compress(img, quality, 'jpg', 800);
-                    jic.upload(compressd, window._BASE_+'/api/photo/upload.json', 'file', file.name,
-                        (res) => {
-                            console.log(res);
+                    var pid = +new Date();
+                    var photo = {
+                        pid: pid,
+                        status: 'uploading',
+                        percent: 0,
+                        src: compressd
+                    };
+                    this.state.photos.push(photo);
+                    this.setState({photos: this.state.photos});
+                    var duringCallback = (e) => {
+                        photo.percent = e.percent;
+                        this.setState({photos: this.state.photos});
+                    }
+                    var errorCallback = (err) => {
+                        photo.status = 'error';
+                        this.setState({
+                            photos: this.state.photos,
+                            showAlert: true,
+                            alertContent: err.error_message || '未知错误'
+                        })
+                    }
+                    var successCallback = (res) => {
+                        if (res.error_code !== 0) {
+                            errorCallback(res);
+                            return;
                         }
+                        photo.pid = res.data.pid;
+                        photo.status = 'done';
+                        this.setState({photos: this.state.photos});
+                    }
+                    jic.upload(
+                        compressd,
+                        window._BASE_+'/api/photo/upload.json',
+                        'file',
+                        file.name,
+                        successCallback,
+                        errorCallback,
+                        duringCallback
                     );
-                   this.state.photos.push({
-                       pid: file.lastModified,
-                       status: 'uploading',
-                       src: compressd
-                   });
-                   this.setState({photos: this.state.photos});
+                    
                },
                options
            );
        });
     }
 
-
-
-
-    errorHandler = (err) => {
-        this.setState({
-            showAlert: true,
-            alertContent: err.error_message || '未知错误'
+    removeImg(photo, index) {
+        Ajax.get('/api/photo/remove/pid/'+photo.pid).end((err, res) => {
+            this.state.photos.splice(index, 1);
+            this.setState({
+                showToast: true,
+                photos: this.state.photos
+            })
+            this.state.toastTimer = setTimeout(()=> {
+                this.setState({showToast: false});
+            }, 1000);
         })
-    }
-
-    opts = {
-        action: window._BASE_ + '/api/photo/upload.json',
-        beforeUpload: (file) => {
-            console.log(file);
-            var reader = new FileReader();
-            reader.onload = function(event) {
-                var i = document.getElementById("source_image");
-                i.src = event.target.result;
-                i.onload = function() {
-                    image_width=$(i).width(),
-                    image_height=$(i).height();
-                    if (image_width > image_height) {
-                        i.style.width="320px";
-                    } else {
-                        i.style.height="300px";
-                    }
-                    i.style.display = "block";
-                    console.log("Image loaded");
-                }
-            };
-            reader.readAsDataURL(file);
-        },
-        onSuccess: (res) => {
-            if (res.error_code !== 0) {
-                this.errorHandler(res);
-                return;
-            }
-            window.pageData.profile.photo.push(res.data);
-            this.setState({photos: window.pageData.profile.photo});
-        },
-        onError: this.errorHandler
     }
 
     hideAlert() {
@@ -120,14 +122,28 @@ export default class MyUploader extends React.Component {
                             <div className="weui_uploader_bd">
                                 <ul className="weui_uploader_files">
                                 {
-                                    this.state.photos.map((photo) => {
-                                        return <li key={photo.pid} className="weui_uploader_file" style={{backgroundImage: `url(${photo.src})`}}>
-                                        </li>
+                                    this.state.photos.map((photo, i) => {
+                                        let content = null;
+                                        
+                                        if (photo.status === 'done') {
+                                            content = (<i onClick={this.removeImg.bind(this, photo, i)} className="weui_icon_cancel remove_img_btn"></i>);
+                                        }
+                                        else if (photo.status === 'error') {
+                                            content = (<i className="weui_icon_warn"></i>);
+                                        } else if (photo.percent && photo.percent < 100) {
+                                            content = photo.percent + '%';
+                                        }
+                                        let className = photo.status === 'done' ? 'weui_uploader_file' : 'weui_uploader_file weui_uploader_status';
+                                        return <li key={photo.pid} className={className} style={{backgroundImage: `url(${photo.src})`}}>
+                                                <div className="weui_uploader_status_content">
+                                                    {content}
+                                                </div>
+                                            </li>
                                     })
                                 }
                                 </ul>
                                 <div className="weui_uploader_input_wrp">
-                                    <input onChange={this.uploadHandle} className="weui_uploader_input" type="file" accept="image/jpg,image/jpeg,image/png,image/gif" multiple="" />
+                                    <input onChange={this.startUpload} className="weui_uploader_input" type="file" accept="image/*" />
                                 </div>
                             </div>
                         </div>
@@ -137,6 +153,7 @@ export default class MyUploader extends React.Component {
                 <Alert title='上传图片失败' buttons={buttons} show={this.state.showAlert}>
                     {this.state.alertContent}
                 </Alert>
+                <Toast show={this.state.showToast}>已删除</Toast>
             </div>
         );
     }
