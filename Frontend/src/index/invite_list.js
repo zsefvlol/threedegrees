018c430ce08667630e1cb51b20df1f2791ccafbc
lol.wen <zsefvlol@163.com>
2016-02-15 11:22:03 +0800

    fix style

--- Frontend/src/index/invite_list.js
+++ Frontend/src/index/invite_list.js
@@ -48,7 +48,7 @@ export default class InviteList extends React.Component {
             };
             render(){
                 return this.state.code.to_uid > 0 && this.state.code.is_single == 1?(
-                    <a onClick={this.handleInviteClick.bind(this)}>{this.state.code.truename} ></a>
+                    <p onClick={this.handleInviteClick.bind(this)}>{this.state.code.truename} ></p>
                 ):(
                     <p className="desc">{this.state.code.truename}</p>
                 );
